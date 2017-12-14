8322798823c0204ff2bac2b342486289f8500159
Marcos Vinicius do Carmo Sousa <marcos.vinicius.wz@gmail.com>
2017-12-13 23:01:07 -0200

    initial money dev bar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,10 @@ class DashboardController < ApplicationController
   def index
     tmp = session[:user]
     @user = User.find_by_id(tmp["id"])
+    getmoney
+  end
+  def getmoney
+    @user.money = 2000
+    return 0
   end
 end
